05b729d89cf2003e8e0ac6152df1f27ef51a98d3
Evelyn Fernandes <evelyn.af22@gmail.com>
2023-08-12 22:19:43 -0300

    PageJsonSerializer
    Criando um serializador personalizado do jackson para a paginação.

---
+++ src/main/java/com/empreget/core/config/jackson/PageJsonSerializer.java
@@ -0,0 +1,30 @@
+package com.empreget.core.config.jackson;
+
+import java.io.IOException;
+
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.domain.Page;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+@JsonComponent
+public class PageJsonSerializer extends JsonSerializer<Page<?>> {
+
+	@Override
+	public void serialize(Page<?> page, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+		
+		gen.writeStartObject();
+		
+		gen.writeObjectField("conteúdo", page.getContent());
+		gen.writeObjectField("paginação", page.getSize());
+		gen.writeObjectField("totalElementos", page.getTotalElements());
+		gen.writeObjectField("totalPages", page.getTotalPages());
+		gen.writeObjectField("pageAtual", page.getNumber());
+		
+		gen.writeEndObject();
+		
+	}
+
+}
